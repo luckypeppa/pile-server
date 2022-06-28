cc90acc837b03aaad52a70ab4e5ab88cba5f17fb
luckypeppa <871565198@qq.com>
2022-06-28 12:08:07 +0800

    minor changes

--- app.js
+++ app.js
@@ -14,16 +14,15 @@ const dbUrl = process.env.DATABASE_URL;
 mongoose
   .connect(dbUrl)
   .then(() => {
-    console.log("Connected to database.");
+    app.use(cors());
+    app.use(morgan("dev"));
+    app.use(express.static("public"));
+    app.use(express.json());
+    app.use(fileUpload());
+    app.use("/blogs", blogRouter);
+    app.use("/auth", authRouter);
+    app.use("/comments", commentRouter);
     app.listen(proces.env.PORT);
+    console.log("Connected to database.");
   })
   .catch((err) => console.log(err));
-
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.static("public"));
-app.use(express.json());
-app.use(fileUpload());
-app.use("/blogs", blogRouter);
-app.use("/auth", authRouter);
-app.use("/comments", commentRouter);
